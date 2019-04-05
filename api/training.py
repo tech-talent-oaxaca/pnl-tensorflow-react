271c71eb402edb900307a5f7931a6bd3f7e2f087
Edgar Talledos <onimenotsuki@gmail.com>
2019-04-05 16:14:54 -0600

    Modelo de entrenamiento finalizado

--- api/training.py
+++ api/training.py
@@ -106,7 +106,7 @@ def clean_up_sentence(sentence):
     sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
     return sentence_words
 
-def bow(sentence, words, show_details=False):
+def bow(sentence, words):
     sentence_words = clean_up_sentence(sentence)
 
     bag = [0] * len(words)
@@ -114,8 +114,6 @@ def bow(sentence, words, show_details=False):
         for i,w in enumerate(words):
             if w == s:
                 bag[i] = 1
-                if show_details:
-                    print ("found in bag: %s" % w)
 
     return(np.array(bag))
 
